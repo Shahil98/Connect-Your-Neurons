2fea277bfe067ea0c1d894ffafdeccbbcfbd5c4f
Shahil <shahilushah@gmail.com>
2020-09-18 11:35:59 -0400

    Modify main.py and index.html, Add bootstrap and layout files, Rename NN.py

--- Flask App/main.py
+++ Flask App/main.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    title = "Neural Network Simulation"
-    return render_template("index.html",title=title)
-if __name__ == "__main__":
-    app.run(debug=True)
+    """
+    Returns index.html when a request is made to "/".
+    """
+    return render_template('index.html', title='Connect Your Neurons')
+
+
+@app.route('/get_keras_model', methods=['POST'])
+def get_keras_model():
+    """
+    Returns a keras model to the
+    user when a request is made to "/get_keras_model"
+    """
+    json_file = request.form["architecture_parameters"]
+    print(eval(json_file))
+    return ("Work in progress")
