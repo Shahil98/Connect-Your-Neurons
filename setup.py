71eb4e5b5cd9bb7e314a4c6233324f6a2beaa50b
Saurabh Patil <saurabhpatil97@hotmail.com>
2020-09-19 12:50:10 -0400

    initial commit

--- setup.py
+++ setup.py
@@ -6,12 +6,6 @@ setup(
     author="Group 10",
     author_email="srpatil4@ncsu.edu",
     description="This is Group 10 project 1 for CS510 Software Engineering",
-    url="https://github.com/Zhuolin0212/Connect-Your-Neurons.git",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.5',
+    url="https://github.com/Zhuolin0212/Connect-Your-Neurons",
+    packages=setuptools.find_packages()
 )
