b949a651cd672f4eb84376024c36b761c2643a35
Saurabh Patil <saurabhpatil97@hotmail.com>
2020-09-19 01:07:36 -0400

    Change the test code

---
+++ test/test_class.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.insert(0, "../Flask\ App")
+from main import create_keras_model
+
+
+class TestClass:
+
+    def test_one(self):
+
+        test_dict = {
+            "number_of_layers": "3",
+            "optimizer": "Adam",
+            "loss_function": "Binary CrossEntropy",
+            "layers": [{"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}]
+        }
+        model_test = create_keras_model(test_dict)
+        assert model_test == test_dict
+
+    def test_two(self):
+
+        test_dict = {
+            "number_of_layers": "3",
+            "optimizer": "Adam",
+            "loss_function": "Binary CrossEntropy",
+            "layers": [{"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}]
+        }
+        model_test = create_keras_model(test_dict)
+        assert model_test == test_dict
+
+    def test_three(self):
+
+        test_dict = {
+            "number_of_layers": "3",
+            "optimizer": "Adam",
+            "loss_function": "Binary CrossEntropy",
+            "layers": [{"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}]
+        }
+        model_test = create_keras_model(test_dict)
+        assert model_test == test_dict
