caafc84987938f1e009a5bdfa37a395f6c93526c
Saurabh Patil <saurabhpatil97@hotmail.com>
2020-09-19 16:01:43 -0400

    Update test files

--- test/test_class.py
+++ test/test_class.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-sys.path.insert(0, "../")
-from code.main import create_keras_model
+from main import create_keras_model
 
 
 class TestClass:
@@ -23,10 +22,10 @@ class TestClass:
     def test_two(self):
 
         test_dict = {
-            "number_of_layers": "3",
+            "number_of_layers": "4",
             "optimizer": "Adam",
             "loss_function": "Binary CrossEntropy",
-            "layers": [{"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}]
+            "layers": [{"number_of_neurons": "613", "activation": "RELU"}, {"number_of_neurons": "613", "activation": "RELU"}, {"number_of_neurons": "613", "activation": "RELU"}]
         }
         model_test = create_keras_model(test_dict)
         for i in range(len(test_dict["layers"])):
@@ -36,12 +35,13 @@ class TestClass:
     def test_three(self):
 
         test_dict = {
-            "number_of_layers": "3",
+            "number_of_layers": "5",
             "optimizer": "Adam",
             "loss_function": "Binary CrossEntropy",
-            "layers": [{"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}]
+            "layers": [{"number_of_neurons": "713", "activation": "RELU"}, {"number_of_neurons": "713", "activation": "RELU"}, {"number_of_neurons": "713", "activation": "RELU"}]
         }
         model_test = create_keras_model(test_dict)
         for i in range(len(test_dict["layers"])):
             assert int(test_dict["layers"][i]["number_of_neurons"]) == model_test["config"]["layers"][i]["config"]["units"]
             assert test_dict["layers"][i]["activation"].lower() == model_test["config"]["layers"][i]["config"]["activation"]
+            
