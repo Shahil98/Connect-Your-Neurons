cbdcb21a16a9d2997d869e6646d8095aba014f6d
Saurabh Patil <saurabhpatil97@hotmail.com>
2020-09-19 02:07:12 -0400

    Add test files

--- test/test_main.py
+++ test/test_main.py
@@ -5,37 +5,15 @@ sys.path.insert(0, "../Flask_App")
 from main import create_keras_model
 
 
-class TestClass:
+def test_one():
 
-    def test_one(self):
+    test_dict = {
+        "number_of_layers": "3",
+        "optimizer": "Adam",
+        "loss_function": "Binary CrossEntropy",
+        "layers": [{"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}]
+    }
+    model_test = create_keras_model(test_dict)
+    assert model_test == test_dict
 
-        test_dict = {
-            "number_of_layers": "3",
-            "optimizer": "Adam",
-            "loss_function": "Binary CrossEntropy",
-            "layers": [{"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}]
-        }
-        model_test = create_keras_model(test_dict)
-        assert model_test == test_dict
-
-    def test_two(self):
-
-        test_dict = {
-            "number_of_layers": "3",
-            "optimizer": "Adam",
-            "loss_function": "Binary CrossEntropy",
-            "layers": [{"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}]
-        }
-        model_test = create_keras_model(test_dict)
-        assert model_test == test_dict
-
-    def test_three(self):
-
-        test_dict = {
-            "number_of_layers": "3",
-            "optimizer": "Adam",
-            "loss_function": "Binary CrossEntropy",
-            "layers": [{"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}, {"number_of_neurons": "513", "activation": "RELU"}]
-        }
-        model_test = create_keras_model(test_dict)
-        assert model_test == test_dict
+test_one()
