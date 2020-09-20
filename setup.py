ebe0a3b88f440029fcfe4115e5eff8c398926769
Saurabh Patil <saurabhpatil97@hotmail.com>
2020-09-20 15:57:07 -0400

    update file

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     author_email="srpatil4@ncsu.edu",
     description="This is Group 10 project 1 for CS510 Software Engineering",
     url="https://github.com/Zhuolin0212/Connect-Your-Neurons",
-    packages=setuptools.find_packages()
+    packages=find_packages()
 )
